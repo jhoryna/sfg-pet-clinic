7a29034e41e159ae287234356549623f01e5adb7
jhoryna <technology@connectedsoftec.com>
2021-01-18 10:42:50 +0100

    POM fixes and Mit sportlichen Grüssen changes

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by jt on 7/21/18.
@@ -86,7 +87,9 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
     @Override
     public List<Owner> findAllByLastNameLike(String lastName) {
 
-        //todo - impl
-        return null;
+        return this.findAll()
+                .stream()
+                .filter(owner -> owner.getLastName().contains(lastName))
+                .collect(Collectors.toList());
     }
 }
